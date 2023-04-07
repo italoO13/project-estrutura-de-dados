0e5f74bf575697dba31443ccd9d8d99fdb7375c1
Italo Iveldo <italoiveldoluiz@gmail.com>
2023-04-07 10:48:55 -0300

    feat: req 1 - implementado função de fila

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,21 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.__len__() == 0:
+            return None
+        return self.queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        print(self.__len__)
+        if (index < 0 or index > self.__len__() - 1):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self.queue[index]
