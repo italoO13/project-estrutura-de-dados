781342841217758146844a2c53c58cb995a1cf81
Italo Iveldo <italoiveldoluiz@gmail.com>
2023-04-10 14:46:12 -0300

    feat: implementado as funções process, remove, file_metadata em file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,33 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(instance.__len__()):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+    file_content = txt_importer(path_file)
+    obj_instance = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_content),
+        "linhas_do_arquivo": file_content,
+    }
+    instance.enqueue(obj_instance)
+    print(obj_instance, file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    path_file = instance.search(0)["nome_do_arquivo"]
+    content = instance.dequeue()
+    if content is None:
+        print("Não há elementos", file=sys.stdout)
+    else:
+        print(f"Arquivo {path_file} removido com sucesso", file=sys.stdout)
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        infos = instance.search(position)
+        print(infos, file=sys.stdout)
+    except IndexError:
+        print("Posição inválida", file=sys.stdout)
