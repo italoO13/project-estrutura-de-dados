ab361ae343f57c3e237b25a2bad5cf5a9b41b241
Italo Iveldo <italoiveldoluiz@gmail.com>
2023-04-10 15:29:52 -0300

    feat: implementado função exists_words

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    words = []
+    for index in range(instance.__len__()):
+        occ_in_file = []
+        instance_content = instance.search(index)
+        file_name = instance_content["nome_do_arquivo"]
+        lines_file = instance_content["linhas_do_arquivo"]
+
+        for n, line in enumerate(lines_file):
+            if word.lower() in line.lower():
+                occ_in_file.append({"linha": n + 1})
+
+        if len(occ_in_file) > 0:
+            words.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_name,
+                    "ocorrencias": occ_in_file,
+                }
+            )
+    return words
 
 
 def search_by_word(word, instance):
